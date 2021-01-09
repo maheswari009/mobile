78b73a4fa49357b9908197d992bcc78f94516279
maheswari009 <76701932+maheswari009@users.noreply.github.com>
2021-01-09 09:09:16 +0530

    Add files via upload

---
+++ pom/pomhome.java
@@ -0,0 +1,19 @@
+package pom;
+
+import org.openqa.selenium.By;
+
+public class pomhome {
+
+    public static By chapt1 = By.xpath("//a[@href=\"/chapter1\"]");
+    public static By chapt2 = By.xpath("//a[@href=\"/chapter2\"]");
+    public  static By chapt3 = By.xpath("//a[@href=\"/chapter3\"]");
+    public static By chapt4 = By.xpath("//a[@href=\"/chapter4\"]");
+    public static By chapt8 = By.xpath("//a[@href=\"/chapter8\"]");
+    public  static By txt = By.xpath("//input[@type=\"text\"]");
+    public  static By button =By.xpath("//input[@type=\"radio\"]");
+    public static By select = By.xpath("//select[@id=\"selecttype\"]");
+    public static By homepage = By.xpath("//a[@href=\"/\"]");
+    public static By button1 =By.xpath("//input[@value=\"Button with name\"]");
+    public static By over = By.xpath("//div[@id=\"hoverOver\"]");
+
+}
